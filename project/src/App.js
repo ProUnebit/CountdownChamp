72eb01f372abcbc458b9c16cc20a6b01cb90c975
ProUnebit <ProUnebit@yandex.ru>
2018-07-18 21:43:09 +0300

    dev version of react app 'Coundown Champ' is completed

--- project/src/App.js
+++ project/src/App.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import Clock from './Clock';
+import './App.css'
+import { Form, FormControl, Button } from 'react-bootstrap';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            deadline: 'December 25, 2018'
+        }
+    }
+
+    changeDeadLine = () => {
+        this.setState({
+            deadline : this.state.newDeadLine
+        })
+    }
+
     render () {
         return (
-            <div>Hi!</div>
+            <div className="app">
+                <div className="app-title">Countdown to {this.state.deadline}</div>
+                <Clock deadline = {this.state.deadline}/>
+                <Form inline>
+                    <FormControl
+                        className="deadline-input"
+                        placholder="new date"
+                        onChange = {event => this.setState({newDeadLine: event.target.value})}
+                    />
+                <Button onClick = {this.changeDeadLine}>Submit</Button>
+                </Form>
+            </div>
         )
     }
 }
