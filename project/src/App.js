ce62c4d68b27e337c7ee68fe9cd4c1f07dcefe86
ProUnebit <ProUnebit@yandex.ru>
2018-07-21 01:00:14 +0300

    add RegExp(vali) for input

--- project/src/App.js
+++ project/src/App.js
@@ -7,20 +7,27 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            deadline: 'June 04, 2019',
+            deadline: 'June 04 2019',
             buttonActivity: true
         }
     }
 
+    regExpForInput = /\b[\Da-z]{3,9}\b\s(0?[1-9]|[12][0-9]|3[01])\s(201[89]|20[2-9][0-9]|2[1-9][0-9][0-9]|[3-9][0-9][0-9][0-9])\b/i;
+
     componentDidMount() {
-        let element = document.getElementsByTagName("Button");
+        let element = document.getElementsByTagName("Button")[0];
         console.log(element);
     }
 
+    // defaultValueRemove = (event) => {
+    //     event.target.value = '';
+    // }
+
     writeInInput = (event) => {
+
         this.setState({
             newDeadLine: event.target.value,
-            buttonActivity: event.target.value !== '' ? false : true
+            buttonActivity: !this.regExpForInput.test(event.target.value)
         })
     }
 
